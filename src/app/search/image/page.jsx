e999267eb027addca544e957895824f2c7f8edb6
Patrick Gip <gippatrick@gmail.com>
2023-03-16 14:59:56 -0700

    Add pagination components

--- src/app/search/image/page.jsx
+++ src/app/search/image/page.jsx
@@ -2,10 +2,12 @@ import ImageSearchResults from "@/components/ImageSearchResults";
 import Link from "next/link";
 
 async function ImageSearchPage({ searchParams }) {
+  const startIndex = searchParams.start || "1";
   await new Promise((resolve) => setTimeout(resolve, 10000));
 
   const response = await fetch(
-    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}&searchType=image`
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}
+    &cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}&searchType=image&start=${startIndex}}`
   );
 
   if (!response.ok) {
